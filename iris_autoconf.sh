e567094e6dee38381f78bfb2803d3f605867eb69
JorgeIvanJH <jorgeivan.jaramillo.herrera@gmail.com>
2026-06-18 17:54:49 +0100

    modified setup files

--- iris_autoconf.sh
+++ iris_autoconf.sh
@@ -42,25 +42,6 @@ Set $namespace="USER"
 Write "Importing ObjectScript packages...",!
 Do $system.OBJ.Import("/usr/irissys/mgr/MLpipeline", "ck")
 
-Write "Loading CSV data to IRIS...",!
-SET exists = ##class(%SYSTEM.SQL.Schema).TableExists("MLpipeline.PointSamples")
-IF 'exists { do ##class(MLpipeline.FeatureStore).%New().UploadCSVtoIRIS("/dur/data/point_samples.csv","USER", "MLpipeline","PointSamples") }
-
-Write "Creating Additional tables for MLpipeline...",!
-Set exists = ##class(%SYSTEM.SQL.Schema).TableExists("MLpipeline.Predictions")
-IF 'exists { do ##class(MLpipeline.FeatureStore).%New().CreatePredictionsTable("USER", "MLpipeline", "Predictions") }
-
-Write "Running First ML Pipeline execution...",!
-set AP = ##class(MLpipeline.AutomatedPipeline).%New()
-set summary = AP.RunPipeline("2026-05-14 00:00:00")
-
-Write "Running First Prediction Service execution...",!
-set predictionsjson = ##class(MLpipeline.PredictionService).Predict("datetime > '2026-05-14 00:00:00'")
-
-Write "Running First Performance Monitoring execution...",!
-set PM = ##class(MLpipeline.PerformanceMonitoring).%New()
-set status = PM.MetricsMonitoring("2026-05-14 00:00:00")
-
 
 Write "IRIS CONFIGURATION COMPLETED.",!
 halt
